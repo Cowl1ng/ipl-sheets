c18c745966567536841ff1dd940accf0750159a6
Cowl1ng <luke.cowling@gmail.com>
2020-08-31 17:08:05 +0100

    Heroku set-up

--- server.js
+++ server.js
@@ -26,6 +26,16 @@ app.use('/api/players', require('./routes/players'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/users', require('./routes/users'))
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+}
+
 // Define port used and connect server
 const PORT = process.env.PORT || 5000
 
